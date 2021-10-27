91cc156bd462b83bd0ca5b5046a824fdaf2cc563
MooooooonLight <zhnyyyy@gmial.com>
2021-10-27 18:21:00 +0800

    更改代码格式

--- baidu_html/baidu.js
+++ baidu_html/baidu.js
@@ -1,32 +1,29 @@
-function radio_status(id){
-    document.getElementById(id).checked="checked";
+function radio_status(id) {
+  document.getElementById(id).checked = "checked";
 }
-function searchSet()
-{
-    document.getElementById("top_right_set_searchset").style.display="block";
-    document.getElementById("advancedSearch").style.display="none";
-    document.getElementById("homeSet").style.display="none";
+function searchSet() {
+  document.getElementById("top_right_set_searchset").style.display = "block";
+  document.getElementById("advancedSearch").style.display = "none";
+  document.getElementById("homeSet").style.display = "none";
 }
-function advSearch()
-{
-    document.getElementById("top_right_set_searchset").style.display="none";
-    document.getElementById("advancedSearch").style.display="block";
-    document.getElementById("homeSet").style.display="none";
+function advSearch() {
+  document.getElementById("top_right_set_searchset").style.display = "none";
+  document.getElementById("advancedSearch").style.display = "block";
+  document.getElementById("homeSet").style.display = "none";
 }
-function homeSet()
-{
-    document.getElementById("top_right_set_searchset").style.display="none";
-    document.getElementById("advancedSearch").style.display="none";
-    document.getElementById("homeSet").style.display="block";
+function homeSet() {
+  document.getElementById("top_right_set_searchset").style.display = "none";
+  document.getElementById("advancedSearch").style.display = "none";
+  document.getElementById("homeSet").style.display = "block";
 }
-function set1(){
-    document.getElementById("top_right_set").style.display="block";
-    searchSet();
+function set1() {
+  document.getElementById("top_right_set").style.display = "block";
+  searchSet();
 }
-function set2(){
-    document.getElementById("top_right_set").style.display="block";
-    advSearch();
+function set2() {
+  document.getElementById("top_right_set").style.display = "block";
+  advSearch();
 }
-function close(){
-    document.getElementById("top_right_set").style.display="none";
+function close() {
+  document.getElementById("top_right_set").style.display = "none";
 }
--- todolist_html/todolist.js
+++ todolist_html/todolist.js
@@ -1,131 +1,114 @@
-function addItem(){
-    var listItem = document.getElementById("item").value;
+function addItem() {
+  var listItem = document.getElementById("item").value;
 
-
-    addOnce(listItem);
+  addOnce(listItem);
 }
-function addList(){
-    document.getElementById("inputDiv").style.display="block";
-}
-
-function addOnce(text)
-{
-
-    text=text.trim();
-    if(text == "")
-    {
-        alert("没事干就别填");
-        return;
-    }
-
-
-    var tab = document.getElementById("table");
-
-    var lines = tab.innerText.split("\n");
-
-    for(var i = 0; i < lines.length; i+=2)
-    {
-        if(text == lines[i])
-        {
-            alert("不要输入重复项");
-            return;
-        }
-    }
-    // debugger;
-    var oTr = document.createElement("tr");
-
-    var oTh = document.createElement("td");
-    var btn = document.createElement("input");
-    var btn1 = document.createElement("input");
-
-    btn.type = "checkbox";
-    btn1.type = "checkbox";
-    btn.className = "finish";
-    btn1.className = "sort";
-    btn1.style.float = "right";
-
-    btn.onclick = finished;
-    btn1.onclick = sort;
-
-    oTh.innerHTML = text;
-
-    oTh.appendChild(btn);
-    oTh.appendChild(btn1);
-
-    oTr.appendChild(oTh);
-
-    tab.appendChild(oTr);
+function addList() {
+  document.getElementById("inputDiv").style.display = "block";
 }
 
-function finished(){
-
-    this.checked=true;
-
-    // sleep(500);
-
-    var tr = this.parentNode.parentNode;
-    var tbody = tr.parentNode;
-    tbody.removeChild(tr);
-}
-
-function sleep(d){
-
-
-    for(var t = Date.now();Date.now() - t <= d;);
+function addOnce(text) {
+  text = text.trim();
+  if (text == "") {
+    alert("没事干就别填");
+    return;
   }
 
-  function ensure(){
+  var tab = document.getElementById("table");
 
-    document.getElementById("inputDiv").style.display="none";
-    debugger;
-    var aa = document.getElementById("listInput");
+  var lines = tab.innerText.split("\n");
 
-    var lines = aa.value.split("\n");
+  for (var i = 0; i < lines.length; i += 2) {
+    if (text == lines[i]) {
+      alert("不要输入重复项");
+      return;
+    }
+  }
+  // debugger;
+  var oTr = document.createElement("tr");
 
-    for(var i = 0; i < lines.length; i++)
-    {
-        addOnce(lines[i]);
+  var oTh = document.createElement("td");
+  var btn = document.createElement("input");
+  var btn1 = document.createElement("input");
+
+  btn.type = "checkbox";
+  btn1.type = "checkbox";
+  btn.className = "finish";
+  btn1.className = "sort";
+  btn1.style.float = "right";
+
+  btn.onclick = finished;
+  btn1.onclick = sort;
+
+  oTh.innerHTML = text;
+
+  oTh.appendChild(btn);
+  oTh.appendChild(btn1);
+
+  oTr.appendChild(oTh);
+
+  tab.appendChild(oTr);
+}
+
+function finished() {
+  this.checked = true;
+
+  // sleep(500);
+
+  var tr = this.parentNode.parentNode;
+  var tbody = tr.parentNode;
+  tbody.removeChild(tr);
+}
+
+function sleep(d) {
+  for (var t = Date.now(); Date.now() - t <= d; );
+}
+
+function ensure() {
+  document.getElementById("inputDiv").style.display = "none";
+  debugger;
+  var aa = document.getElementById("listInput");
+
+  var lines = aa.value.split("\n");
+
+  for (var i = 0; i < lines.length; i++) {
+    addOnce(lines[i]);
+  }
+}
+
+function sort() {
+  // debugger;
+
+  this.checked = false;
+
+  var aa = document.getElementById("table");
+
+  var td = this.parentNode;
+
+  var lines = aa.innerText.split("\n");
+
+  var liness = aa.innerText;
+
+  var temp = lines[0];
+
+  lines[0] = td.innerText;
+
+  for (var i = 2; i < lines.length; i += 2) {
+    if (lines[i] == td.innerText) {
+      lines[i] = temp;
     }
   }
 
-  function sort(){
-
-    // debugger;
-
-    this.checked = false;
-
-    var aa = document.getElementById("table");
-
-    var td =  this.parentNode;
-
-    var lines = aa.innerText.split("\n");
-
-    var liness=aa.innerText;
-
-    var temp = lines[0];
-
-
-    lines[0] = td.innerText;
-
-    for(var i = 2; i < lines.length; i+=2)
-    {
-        if(lines[i] == td.innerText)
-        {
-            lines[i] = temp;
-        }
-    }
-
-    for(var i = 0; i < lines.length; i+=2)
-    {
-        aa.children[i/2].children[0].childNodes[0].data= lines[i];
-    }
-
+  for (var i = 0; i < lines.length; i += 2) {
+    aa.children[i / 2].children[0].childNodes[0].data = lines[i];
   }
+}
 
-  function OnTextChanged() {
-    if (event.keyCode == 13) {//判断是否为回车键，Event是window对象的一个属性，是全局的。
-        event.keyCode = 0;//屏蔽回车键
-        event.returnValue = false;
-        alert("Hello world!");
-    }
+function OnTextChanged() {
+  if (event.keyCode == 13) {
+    //判断是否为回车键，Event是window对象的一个属性，是全局的。
+    event.keyCode = 0; //屏蔽回车键
+    event.returnValue = false;
+    alert("Hello world!");
+  }
 }
