33b24cae09cf3994410a1cca3ad7682123683c4b
MooooooonLight <zhnyyyy@gmial.com>
2021-11-01 10:45:33 +0800

    大致完成

--- slideshow_html/slideshow.js
+++ slideshow_html/slideshow.js
@@ -1,5 +1,6 @@
 let index = 0;
 let lock = true;
+let autoplay;
 /*
  *@functionName:
  *@Author: 张浩楠
@@ -33,7 +34,7 @@ function clickRight() {
     lock = true;
   }, 500);
 
-  bottomShow();
+  bottomShow("right");
 }
 /*
  *@functionName:
@@ -70,10 +71,12 @@ function clickLeft() {
   setTimeout(function () {
     lock = true;
   }, 500);
+
+  bottomShow("left");
 }
 
-// 自动轮播
-let autoplay = setInterval(() => {
+// 自动轮播;
+autoplay = setInterval(() => {
   clickRight();
 }, 2000);
 
@@ -115,20 +118,90 @@ function rollContinue() {
  *@return:
  *@Description: 设置下标显示
  */
-function bottomShow() {
-  debugger;
+function bottomShow(direction) {
   let list = document.getElementById("main-pointer");
+  let images = document.querySelector(".img-list");
 
   list = list.getElementsByTagName("li");
 
   for (var i = 0; i < list.length; i++) {
-    if (index === 4) {
-      list[0].style.backgroundColor = "white";
+    if (images.style.marginLeft === "-2000px") {
+      if (direction === "right") {
+        list[0].style.backgroundColor = "white";
+      } else {
+        list[0].style.backgroundColor = "blue";
+        list[3].style.backgroundColor = "white";
+        continue;
+      }
     }
-    if (i === index) {
+    if (images.style.marginLeft === i * -500 + "px") {
       list[i].style.backgroundColor = "white";
     } else {
       list[i].style.backgroundColor = "blue";
     }
   }
+}
+/*
+ *@functionName:
+ *@Author: 张浩楠
+ *@Date: 2021-11-01 09:33:30
+ *@param in:
+ *@param out:
+ *@return:
+ *@Description: 点击下标事件
+ */
+function clickBottom(id) {
+  let images = document.querySelector(".img-list");
+  var curPos = parseInt(images.style.marginLeft);
+  if (curPos === "") {
+    curPos = 0;
+  }
+
+  if (id === "p1") {
+    var targetPos = 0;
+  }
+  if (id === "p2") {
+    var curPos = images.style.marginLeft;
+    var targetPos = -500;
+  }
+  if (id === "p3") {
+    var curPos = images.style.marginLeft;
+    var targetPos = -1000;
+  }
+  if (id === "p4") {
+    var curPos = images.style.marginLeft;
+    var targetPos = -1500;
+  }
+  clickMove(curPos, targetPos);
+}
+/*
+ *@functionName:
+ *@Author: 张浩楠
+ *@Date: 2021-11-01 09:45:52
+ *@param in:
+ *@param out:
+ *@return:
+ *@Description: 点击下标移动函数
+ */
+function clickMove(curPos, tarPos) {
+  debugger;
+  clearInterval(autoplay);
+  let images = document.querySelector(".img-list");
+  var speed;
+  if (curPos > tarPos) {
+    speed = -5;
+  } else {
+    speed = 5;
+  }
+
+  autoplay = setInterval(() => {
+    debugger;
+    curPos += speed;
+    // images.style.marginLeft = curPos + "px";
+    // if (images.style.marginLeft === tarPos + "px") {
+    //   clearInterval(autoplay);
+    // }
+  }, 0);
+
+  // rollContinue();
 }
